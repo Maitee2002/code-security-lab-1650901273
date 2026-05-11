cc25def9aade7aa04daf83b0f72661d0fc458b4b
maitee2002 <thatthada.nutc@bumail.net>
2026-05-11 21:16:14 +0000

    fix bandit issues

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ import subprocess
 import ast
 import json
 
-# FIX B602: ไม่ใช้ shell=True
 def run_cmd(host):
     return subprocess.check_output(['ping', '-c', '1', host], shell=False)
 
-# FIX B307: ไม่ใช้ eval
 def calc(expr):
     return ast.literal_eval(expr)
 
-# FIX B301: ไม่ใช้ pickle
 def load(data):
     return json.loads(data)
